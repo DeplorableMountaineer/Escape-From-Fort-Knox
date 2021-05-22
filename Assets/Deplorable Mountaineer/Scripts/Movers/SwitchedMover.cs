8ffbe3a3c3f84052394195232d4435727e4edceb
DeplorableMountaineer <tdvance@deplorablemountaineer.com>
2021-05-21 21:56:22 -0400

    Rider added ignored files Modified basic shader to change tiling direction added blue glow material Added laser canon added switched mover and trigger switch Deleted redundant files Added gold brick material Started building level

---
+++ Assets/Deplorable Mountaineer/Scripts/Movers/SwitchedMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Deplorable_Mountaineer.Movers {
+    public class SwitchedMover : MonoBehaviour {
+        [SerializeField] private Transform endPosition;
+        [SerializeField] private float speed = 1;
+        private bool _activated = false;
+        private Vector3 _endPosition;
+
+        private void Awake(){
+            _endPosition = endPosition.position;
+        }
+
+        private void FixedUpdate(){
+            if(!_activated) return;
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, _endPosition,
+                speed*Time.fixedDeltaTime);
+            transform.position = newPosition;
+            if(Vector3.Distance(newPosition, _endPosition) < speed*Time.fixedDeltaTime)
+                enabled = false;
+        }
+
+        public void OnActivate(){
+            _activated = true;
+        }
+    }
+}
---
+++ Assets/Deplorable Mountaineer/Scripts/Switches/Trigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Deplorable_Mountaineer.Switches {
+    public class Trigger : MonoBehaviour {
+        [SerializeField] private UnityEvent onActivate;
+        [SerializeField] private int numActivations = 1;
+        [SerializeField] private AudioSource activationSound;
+
+        private void OnTriggerEnter(Collider other){
+            if(!other.CompareTag("Player")) return;
+            if(numActivations <= 0) return;
+            numActivations--;
+            onActivate?.Invoke();
+            if(activationSound) activationSound.Play();
+        }
+    }
+}
